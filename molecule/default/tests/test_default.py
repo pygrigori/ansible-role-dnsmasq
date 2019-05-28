44c04cab4b5ee7776c629720e63d5411502c93cc
Grigori Py <pygrigori@gmail.com>
2019-05-28 16:51:02 +0300

    molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,9 +7,35 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('package', [
+    'dnsmasq',
+])
+def test_pkg_installed(host, package):
+    package = host.package(package)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('service', [
+    'dnsmasq'
+])
+def test_service_is_enabled(host, service):
+    service = host.service(service)
+
+    assert service.is_enabled
+
+
+def test_config_exists(host):
+    file = host.file('/etc/dnsmasq.conf')
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+
+    print(file.content)
+
+
+def test_config_is_valid(host):
+    cmd = host.run('dnsmasq --test --conf-file=/etc/dnsmasq.conf')
+
+    assert not cmd.rc
